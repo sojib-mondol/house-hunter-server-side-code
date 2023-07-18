499b851cb7f6e244fbcc896b5d34ff416eae86c4
sojib-sudo <mdsojibmandol@gmail.com>
2023-07-18 12:24:42 +0600

    JWT & login api

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
+
+const secret = process.env.ACCESS_TOKEN_SECRET;
 
 const port = process.env.PORT || 5000;
 
@@ -13,18 +17,39 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zmcxwrx.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri);
-console.log(uri);
+//console.log(uri);
 
 async function run() {
-  // Connect to MongoDB && collection 
+  //Connect to MongoDB && collection
   await client.connect();
   const db = client.db("house-hunter");
   const usersCollection = db.collection("users");
+
+  // gettung users data
+  app.get("/users", async (req, res) => {
+    try {
+      await client.connect();
+      const db = client.db("house-hunter");
+      const usersCollection = db.collection("users");
+      const query = {};
+      const users = await usersCollection.find(query).toArray();
+      res.send(users);
+    } catch (error) {
+      console.error("get user error:", error);
+      res.status(500).json({ message: "Internal server error" });
+    } finally {
+    }
+  });
 
   // Register endpoint
   app.post("/register", async (req, res) => {
     try {
       const { fullName, role, phoneNumber, email, password } = req.body;
+
+      // Connect to MongoDB
+      await client.connect();
+      const db = client.db("house-hunter");
+      const usersCollection = db.collection("users");
 
       // Check if user already exists
       const existingUser = await usersCollection.findOne({ email });
@@ -44,15 +69,49 @@ async function run() {
       // Save the user to the database
       const result = await usersCollection.insertOne(newUser);
       // Generate a JWT token
-      const token = jwt.sign({ userId: result.insertedId }, "your-secret-key");
-      // Return the token to the client
+      const token = jwt.sign({ userId: result.insertedId }, secret);
+      //Return the token to the client
+
       res.json({ token });
+      //res.send(result);
     } catch (error) {
       console.error("Error registering user:", error);
       res.status(500).json({ message: "Internal server error" });
     } finally {
-      // Close the MongoDB connection
-      await client.close();
+    }
+  });
+
+  // Login endpoint
+  app.post("/login", async (req, res) => {
+    try {
+      const { email, password } = req.body;
+
+      // Connect to MongoDB
+      await client.connect();
+      const db = client.db("house-hunter");
+      const usersCollection = db.collection("users");
+
+      // Find the user in the database
+      const user = await usersCollection.findOne({ email });
+      if (!user) {
+        return res.status(401).json({ message: "Invalid credentials" });
+      }
+
+      // Verify the password
+      const isPasswordValid = await bcrypt.compare(password, user.password);
+      if (!isPasswordValid) {
+        return res.status(401).json({ message: "Invalid credentials" });
+      }
+
+      // Generate a JWT token
+      const token = jwt.sign({ userId: user._id }, secret);
+
+      // Return the token to the client
+      res.json({ token });
+    } catch (error) {
+      console.error("Error logging in user:", error);
+      res.status(500).json({ message: "Internal server error" });
+    } finally {
     }
   });
 }
